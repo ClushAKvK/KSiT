966ca5c27a4e3f26233bbcb47d1e5d1716167237
Aleksandr Shirko <AShirko@nexign.com>
2025-04-15 00:55:38 +0300

    Test commit

--- src/com/company/Template/SimulationResult.java
+++ src/com/company/Template/SimulationResult.java
@@ -1,13 +1,13 @@
 package com.company.Template;
 
 public class SimulationResult {
-    public int tasksProcessed;
+    public int tasksProcessed_1;
     public double busyTime;
     public double totalTime;
     public double totalDelay;
 
     public SimulationResult(int tasksProcessed, double busyTime, double totalTime, double totalDelay) {
-        this.tasksProcessed = tasksProcessed;
+        this.tasksProcessed_1 = tasksProcessed;
         this.busyTime = busyTime;
         this.totalTime = totalTime;
         this.totalDelay = totalDelay;
